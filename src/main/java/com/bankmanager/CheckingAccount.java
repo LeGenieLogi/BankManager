74407623fb9b64915cc3a666ed1d8dc0892db9f3
LGL925 <kouakoujeanelisee592@gmail.com>
2026-06-02 11:15:07 +0000

    Creation de la composante CheckingAccount

---
+++ src/main/java/com/bankmanager/CheckingAccount.java
@@ -0,0 +1,34 @@
+package com.bankmanager;
+
+/**
+ * Compte courant.
+ * Il ajoute une limite de decouvert au compte bancaire de base.
+ */
+public class CheckingAccount extends BankAccount {
+    private double limit;
+
+    public CheckingAccount() {
+    }
+
+    public CheckingAccount(String account, double balance, double limit) {
+        super(account, balance);
+        this.limit = limit;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckingAccount{" +
+                "account='" + getAccount() + '\'' +
+                ", balance=" + getBalance() +
+                ", limit=" + limit +
+                '}';
+    }
+}
